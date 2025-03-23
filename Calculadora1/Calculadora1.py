0f33992db5764fde36475d1aca969dc60a7a8a6e
yelmastet <davidtraker07@gmail.com>
2025-03-23 15:16:26 -0500

    test of V.S.C.

--- Calculadora1/Calculadora1.py
+++ Calculadora1/Calculadora1.py
@@ -112,8 +112,6 @@ def run():
         print("Operación no válida. Inténtelo de nuevo.")
 
     # **** ****
-
-
 # **** Conserve este condicional para ejecutar el programa directamente
 if __name__ == "__main__":
     run()
